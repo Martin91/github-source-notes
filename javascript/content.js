4eb8feed733287405a559bdebd11b3d68a8343ec
Martin <hongzeqin@gmail.com>
2014-06-10 10:22:40 +0800

    enable to prepend add-note image

--- javascript/content.js
+++ javascript/content.js
@@ -1,4 +1,18 @@
 (function(){
+  var startLine, endLine;
+  var addNoteNode = function() {
+    var _addNoteNode = document.createElement('a');
+    _addNoteNode.className = 'add-note';
+
+    var addNoteImageNode = document.createElement('img');
+    // https://developer.chrome.com/extensions/i18n#overview-predefined
+    addNoteImageNode.src = 'chrome-extension://__MSG_@@extension_id__/images/note_add.png';
+
+    _addNoteNode.appendChild(addNoteImageNode);
+
+    return _addNoteNode;
+  };
+
   var extractNumberFromString = function(str) {
     return parseInt(str.replace(/\D+/, ''));
   };
@@ -19,21 +33,36 @@
 
     return lines.reverse();
   }
+  var lastSelectedLine = function() {
+    return document.getElementById("L" + endLine);
+  }
+  var prependAddNoteNode = function() {
+    var targetLine = lastSelectedLine();
+    targetLine.insertBefore(addNoteNode(), targetLine.childNodes[0]);
+  }
+  var clearAddNoteNode = function() {
+    var targetLine = lastSelectedLine();
+    targetLine.removeChild(targetLine.childNodes[0]);
+  }
 
   var lineNumsColumnContainer = document.getElementsByClassName('blob-line-nums')[0];
 
   lineNumsColumnContainer.addEventListener('click', function(event){
+    if(endLine) { clearAddNoteNode(); }
+
     var selectedLines = analysisSelectedLines();
 
     if(selectedLines.length === 0) { return; }
 
-    var startLine = selectedLines[0];
-    var endLine = selectedLines[selectedLines.length - 1];
+    startLine = selectedLines[0];
+    endLine = selectedLines[selectedLines.length - 1];
 
     if(startLine === endLine) {
       alert("Selected line is:" + startLine);
     } else {
       alert("Selected lines is from " + startLine + ' to ' + endLine);
     }
+
+    prependAddNoteNode();
   });
 })();
